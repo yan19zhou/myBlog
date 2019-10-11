84536a4dde5ee1bbd883b0a23b40a03dd54b308d
zhouyan <zhouyanshengwei@163.com>
2019-10-12 00:02:37 +0800

    搭建简单node服务

--- node/web/services1.js
+++ node/web/services1.js
@@ -1,7 +1,48 @@
 const http = require('http');
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
 
 http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html;charset='utf-8'" })
-    res.write("something");
-    res.end();
+    // 读取文件然后显示到页面
+    // 获取请求路径
+    let pathName = url.parse(req.url).pathname;
+    if (pathName == '/') {
+        pathName = '/index.html' //  默认加载首页
+    }
+    // 获取文件后缀
+    let extname = path.extname(pathName);
+
+    //读取文件
+
+    fs.readFile('static' + pathName, (err, data) => {
+        if (err) {
+            fs.readFile('static/404.html', (err, data404) => {
+                res.writeHead(404, {
+                    "Content-Type": "text/html;charset='utf-8'"
+                })
+                res.write(data404);
+                res.end();
+            })
+        } else {
+            res.writeHead(200, {
+                "Content-Type": "" + getExtName(extname) + ";charset='utf-8'"
+            })
+            res.write(data);
+            res.end();
+        }
+    })
 }).listen(8080)
+
+function getExtName(extname) {
+    switch (extname) {
+        case '.html':
+            return 'text/html'
+        case '.css':
+            return 'text/css'
+        case '.js':
+            return 'text/javascript'
+        default:
+            return 'text/html'
+    }
+}
---
+++ node/web/static/js/main.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    function changeColor() {
+        document.getElementById("btn").click = function() {
+            document.getElementsByClassName("title")[0].style.color = '#ccc';
+        }
+    }
+}
